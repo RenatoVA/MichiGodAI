8880ef35c22557ba16e29839bddf02d148872370
RenatoVA <renato.vivas2001@gmail.com>
2024-11-12 10:24:52 -0500

    integracion con docker

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(3000);
-  console.log(`Servidor corriendo en http://localhost:3000`);
+  console.log(`Servidor corriendo en http://localhost:3000/api`);
 }
 bootstrap();
